d9bd432eafd82a1f22e2ae252640f30f76499dda
Jordi <jordiigarciatorres@gmail.com>
2024-09-12 10:10:19 +0200

    Add functionality and structure of the program

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const container = document.querySelector(".container");
+const button = document.querySelector(".btn");
+createDefaultGrid();
+
+button.addEventListener("click", e =>{
+    const input = prompt("Enter how many squared cells you want: ");
+    createNewGrid(input);
+});
+
+function createRows(rowNum){
+    let row;
+    for(let i = 0; i < rowNum; i++){
+        row = document.createElement("div");
+        row.classList.add("row")
+        container.appendChild(row);
+    }
+
+}
+
+function createCells(cellNum){
+    let cell;
+    const rows = document.querySelectorAll(".row");
+    rows.forEach(row => {
+        for(let i = 0; i < cellNum; i++){
+            cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+            cell.addEventListener("mouseover", e => {
+                e.target.style.background = "purple";
+            })
+        }
+    });
+}
+
+
+function createDefaultGrid(){
+    createRows(16);
+    createCells(16);
+}
+
+function removeGrid(){
+    const rows = document.querySelectorAll(".row");
+    rows.forEach(row => 
+        row.remove()
+    )
+    console.log("Last grid has been removed sucessfully.");
+}
+
+function createNewGrid(cellNum){
+    removeGrid();
+    createRows(cellNum);
+    createCells(cellNum);
+}
